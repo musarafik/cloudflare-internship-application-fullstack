53953511fb525de92a15877399c6f741ce1feea6
Musa Rafik <musa_rafik@utexas.edu>
2020-04-15 22:30:10 -0500

    added random number generator to simulate a/b testing

--- musa-application/index.js
+++ musa-application/index.js
@@ -3,21 +3,21 @@ addEventListener('fetch', event =>{
 });
 
 /**
- *  Send fetch request to url for assignment and return json parsed response
+ *  Send fetch request to url for assignment and return response
+ *  Use random number generator to simulate A/B testing by switching between two pages to show based off generated value
  * @param {Request} request 
  */
 async function handleRequest(request){
   let urlArray = await fetchUrl('https://cfw-takehome.developers.workers.dev/api/variants');
   urlArray = urlArray.variants;
-  console.log(urlArray);
-  // let url1Response =  await fetchUrl(urlArray[0]);
-  // let url2Response = await fetchUrl(urlArray[1]);
+  let responseNumber = Math.round(Math.random()); // response to render will be randomly selected between 0 and 1 in the array
 
-  return new Response("hello");
+  return new Response(await fetchUrl(urlArray[responseNumber]));
 }
 
+
 /**
- * Returns json response to fetch request
+ * Returns response from fetch request of given url (checks if response type is json or html)
  * @param url - Used for fetch request
  */
 async function fetchUrl(url){
@@ -27,7 +27,13 @@ async function fetchUrl(url){
 
   try{
     const response = await fetch(url, urlRequestParams);
-    return response.json();
+    let contentType = response.headers.get("content-type");
+    if(contentType === 'application/json'){ // If json response we have to parse it
+      return response.json();
+    }
+    else if(contentType === 'text/html'){ // HTML response
+      return response.text();
+    }
   }
   catch(err){
     console.log(err);
