505e26d7edb5c263de4d28cd2178172f71de21ff
Musa Rafik <musa_rafik@utexas.edu>
2020-04-16 16:25:52 -0500

    got html rewriter to change title and link

--- musa-application/index.js
+++ musa-application/index.js
@@ -1,6 +1,27 @@
-addEventListener('fetch', event =>{
-  event.respondWith(handleRequest(event.request))
-});
+ class AttributeRewriter {
+  constructor(attributeName) {
+    this.attributeName = attributeName
+  }
+ 
+  element(element) {
+    const attribute = element.getAttribute(this.attributeName)
+    if (attribute) {
+      element.setAttribute(
+        this.attributeName,
+        attribute.replace('https://cloudflare.com', 'linkedin.com/in/musa-rafik') // Change the link from cloudflare to my linkedin account
+      )
+      element.setInnerContent("Check out Musa's Linkedin!"); // Change the text for link
+    }
+    else{
+      element.setInnerContent("This is Musa's application for the full stack internship"); // Change title
+    }
+  }
+}
+
+// Change the link to point to my linkedin account instead of cloudflare and change title from variant # to this is musa's application ...
+const rewriter = new HTMLRewriter()
+  .on('a', new AttributeRewriter('href'))
+  .on('title', new AttributeRewriter('title'));
 
 /**
  *  Get two urls from first url and send a response from one of the urls 
@@ -20,16 +41,17 @@ async function handleRequest(request){
   // Get cookie and check which page to show based off value 
   const cookie = request.headers.get('cookie'); 
   if(cookie && cookie.includes(`${NAME}=group1`)){
-    return response1;
+    return rewriter.transform(response1);
   }
   else if(cookie && cookie.includes(`${NAME}=group2`)){
-    return response2;
+    return rewriter.transform(response2);
   }
   else{ // New user so create a cookie for them 
     let group = Math.random() < 0.5 ? 'group1': 'group2'; // Simualate A/B testing style
     let response = group === 'group1' ? response1: response2;
     response.headers.append('Set-Cookie', `${NAME}=${group}; path=/; domain=musarafik.workers.dev; SameSite=Lax;`);
-    return response;
+    // return response;
+    return rewriter.transform(response);
 
   }
 }
@@ -57,3 +79,7 @@ async function fetchUrl(url){
     console.log(err);
   }
 }
+
+addEventListener('fetch', event =>{
+  event.respondWith(handleRequest(event.request))
+});
